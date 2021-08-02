f03c5c3de1aeffe57bd2678060324e7e6059aefb
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-03 02:28:09 +0600

    nasted added

---
+++ nasted-condition.js
@@ -0,0 +1,16 @@
+
+
+var danishPrice = 50;
+var butterBreadPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 7;
+
+if(danishPrice < myBudget){
+    console.log('danish khamu');
+}
+else if(butterBreadPrice < myBudget){
+    console.log('khamu na butterbon');
+}
+else{
+    console.log('kisui khamo na ami roja');
+}
