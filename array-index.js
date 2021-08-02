9288f26f8cdc4d97c6cce09c744e007284dae4a1
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-02 11:00:27 +0600

    js fundamentals

---
+++ array-index.js
@@ -0,0 +1,10 @@
+
+var bookPages = ['hablu', 'bolod', 'choddogusti', 'bettery', 'peracitamol', 'gymnesium'];
+
+var bettayIndex = bookPages.indexOf('bettery');
+var secondIndex = bookPages[2];
+bookPages[1] = "labib khan"
+
+console.log(bettayIndex);
+console.log(secondIndex);
+console.log(bookPages);
---
+++ array.js
@@ -0,0 +1,7 @@
+
+
+var friendsAge = [11, 12, 43, 23];
+var naikas = ['porimoni', 'kriti', 'alia'];
+
+console.log(naikas);
+console.log(friendsAge.length);
